1eb3ca2f0201c4ca60fe80135c6d66e97ad04530
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2024-05-22 22:57:15 +0000

    add copy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import RefferalForm from "./components/ReferralForm";
 import TFTCLogo from "/tftc-logo.svg";
+import QuestionMark from "/question-mark.svg";
 
 export default function App() {
   return (
@@ -9,6 +10,27 @@ export default function App() {
         <img src={TFTCLogo} alt="TFTC Logo" />
       </div>
       <h1>Referral Form</h1>
+      <div>
+        {/* <div className="question-mark-container">
+          <img
+            style={{ width: "25px" }}
+            src={QuestionMark}
+            alt="Question Mark"
+          />
+        </div> */}
+        <ol>
+          <li>
+            Think of someone in your network who isn't a member of the TFTC
+            community and refer them to our email list using the form linked
+            below.
+          </li>
+          <li>
+            If that person stays subscribed to the newsletter and opens three
+            issues you will both receive sats. A link to redeem your TFTC
+            rewards will be sent directly to your email inbox.
+          </li>
+        </ol>
+      </div>
       <RefferalForm />
     </main>
   );
